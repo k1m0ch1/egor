c81d419406d8106ffb285747524d71b07d5201bd
Dimas Satrio <dimassrio@gmail.com>
2015-10-19 03:44:14 +0700

    fixing role controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,35 +6,59 @@ use DB;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 
 class RoleController extends Controller
 {
     public function form(Request $request){
         $as = $request->input('as');
+
         $rS[0] = new \StdClass();
         $rS[0]->id = "xxx";
         $rS[0]->name = "";
         $rS[0]->display_name = "";
         $rS[0]->description = "";
+
         if($as!="add"){
             $rS = DB::table('roles')->where('id', $request->input('id'))->get();
         }
-        
         return view('_layout.form-role-backend', compact('rS', 'as'));
     }
 
     public function save(Request $request){
         $as = $request->input('as');
-        $dataSave = Array('name'=>$request->input('name'),
-                           'display_name'=>$request->input('displayname'),
-                           'description'=>$request->input('description'));
-        if($as=="edit"){
-            $hasil = DB::table('roles')->where('id', $request->input('id'))->update($dataSave);
-        }else{
-            $hasil = DB::table('roles')->insert($dataSave);
-        }
+        $role = new Role;
+        $validator = \Validator::make($request->all(), $role->getRules());
+        $results = new \StdClass;
 
-        return $hasil==true?"success save":"fail save";
+        if($validator->passes){
+            if($request->has('id')){
+                $role = Role::find($id);
+                $role->name = $request->name;
+                $role->display_name = $request->display_name;
+                $role->description = $request->description;
+                $role->save();
+                $results->info = 'role create';
+            }else{
+                $role = new Role;
+                $role->name = $request->name;
+                $role->display_name = $request->display_name;
+                $role->description = $request->description;
+                $role->save();
+                $results->info = 'role edit';
+            }
+            $results->status = 1;
+            $results->result = $role;
+        }else{
+            $results->status = 0;
+            $result = array();
+            foreach ($validator->errors() as $key => $err) {
+                array_push($result, $err);
+            }
+            $results->result = $result;
+        }
+        
+        return response()->json($results);
     }
 
     public function show(){
--- resources/views/_layout/tabel-roles.blade.php
+++ resources/views/_layout/tabel-roles.blade.php
@@ -1,10 +1,12 @@
 @foreach($result as $rS)
-                        <tr>
-                          <td>{{ $rS->name }}</td>
-                          <td>{{$rS->display_name}}</td>
-                          <td><div class="tools" align='center'>
-                                <a class="fa fa-edit" id="editRule-{{ $rS->id }}"></a>&nbsp;
-                                <a class="fa fa-trash-o"></a>
-                              </div></td>
-                        </tr>
+	<tr>
+		<td>{{ $rS->name }}</td>
+		<td>{{$rS->display_name}}</td>
+		<td>
+			<div class="tools" align='center'>
+				<button type="button" id="editRule-{{ $rS->id }}"><i class="fa fa-edit"></i></button type="button">
+				<button type="button" id="deleteRule-{{$rS->id}}"><i class="fa fa-trash-o"></i></button type="button">
+			</div>
+		</td>
+	</tr>
 @endforeach
