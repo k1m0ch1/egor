dad89096ccae3f0898c010bd8906c0f2a507d868
k1m0ch1 <yahya.kimochi@gmail.com>
2015-11-04 12:52:23 +0700

    fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -76,13 +76,13 @@ class PagesController extends Controller
         }
 
         $datanyah = array();
-        if(ParentFrontpage::find($permissions->action)->get()>0){
           foreach($resultPermission as $permissions){
               if($permissions->type == 'app'){
+                if(ParentFrontpage::find($permissions->action)->get()>0){
                   array_push($datanyah, ParentFrontpage::find($permissions->action));
+                }
               }
           }
-        }
 
         return view('frontend.index', compact('result1', 'bah', 'datanyah', 'h', 'w', 'bg', 'footer', 'resultPermission'));
     }
