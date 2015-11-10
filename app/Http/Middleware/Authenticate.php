7d150d33f61480acb71cea53a9751004969b528b
k1m0ch1 <yahya.kimochi@gmail.com>
2015-11-10 02:34:35 +0000

    add exception static permission

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -81,7 +81,8 @@ class Authenticate
                       }
 
                       if($currentRoute=="admin/form" || $currentRoute=="admin/filesList/{id}" || $currentRoute=="admin/setGrid" ||
-                        $currentRoute=="admin/users[edit:show]" || $currentRoute=="admin/users[edit:save]" ){
+                        $currentRoute=="admin/users[edit:show]" || $currentRoute=="admin/users[edit:save]" || $currentRoute=="news"
+                        || $currentRoute=="news/{id}" ){
                         $pass=true;
                       }
                   }
